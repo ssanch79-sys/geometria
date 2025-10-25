4bd1095fb0b18b6bdf5b7310c023ca95d977e6e5
ssanch79-sys <ssanch79@xtec.cat>
2025-10-25 16:53:54 +0200

    fix: create working index.tsx

---
+++ index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+
+const rootElement = document.getElementById('root');
+if (!rootElement) throw new Error('Root not found');
+
+const root = ReactDOM.createRoot(rootElement);
+root.render(React.createElement(React.StrictMode, null, React.createElement(App)));
